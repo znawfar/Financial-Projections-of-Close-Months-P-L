e93e3008d951f0039710950f15ae6cb062856697
Zinat Nawfar <95911833+znawfar@users.noreply.github.com>
2023-02-09 09:35:27 +0000

    version 1.0.3

--- Query3.sql
+++ Query3.sql
@@ -4,15 +4,17 @@
 -- Revenue - Discount = Net Sales - Cost of Goods Solds = Cross Profit - Expenses = Net Profit
 
 
-------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
 --Part 1: Net Sales
 SELECT (Pay.Payable - Credit.Credit_Dis) AS Net_Sales
 From
 	(SELECT Sum(Amount_Sales) as Payable FROM Sales) AS Pay,
 	(SELECT Sum(Amount_Discounts) as Credit_Dis FROM Discounts) AS Credit;
-
-
+	
 -------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+
 --Part 2: Cross Profit 
 SELECT (NetSales.Net_Sales - GoodSolds.Good_Solds) AS Cross_Profit 
 From
@@ -22,8 +24,8 @@ From
 		(SELECT Sum(Amount_Discounts) as Credit_Dis FROM Discounts) AS Credit) AS NetSales,
 	(SELECT Sum(Amount_Cost_Solds) as Good_Solds FROM Cost_of_Good_Solds) AS GoodSolds;
 	
-	
---------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------	
+---------------------------------------------------------------------------------------------------------
 --Part 3: Net Profit
 SELECT (CrossProfit.Cross_Profit - OperatingExpenses.OperatingEx) AS Net_Profit 
 From
@@ -36,5 +38,7 @@ From
 		(SELECT Sum(Amount_Cost_Solds) as Good_Solds FROM Cost_of_Good_Solds) AS GoodSolds) AS CrossProfit,
 	(SELECT Sum(Amount_Operating_Expenses) as OperatingEx FROM Operating_Expenses) AS OperatingExpenses;
 
+---------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
 
 
